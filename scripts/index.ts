037fd6482a18570f351fc008cd2e94b282fd209b
taborkomaria <taborkomaria@gmail.com>
2017-09-13 19:25:21 +0300

    recipe app

--- scripts/index.ts
+++
@@ -1,8 +0,0 @@
-import {render} from 'preact';
-// import 'preact/devtools';
-import AppRoot from './components/AppRoot';
-import './data/init';
-import subscribe from './data/subscribe';
-
-subscribe();
-render( AppRoot, document.body );
