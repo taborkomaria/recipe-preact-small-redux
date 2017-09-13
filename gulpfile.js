5b76bbdf5160b620cb80f6a61404995eb7351f76
taborkomaria <taborkomaria@gmail.com>
2017-09-13 19:27:43 +0300

    recipe app

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const Gulp = require( 'gulp' );
+
+function cleanExit()
+{
+	process.exit();
+}
+
+// Catch ctrl-c
+process.on( 'SIGINT', cleanExit );
+// Catch kill
+process.on( 'SIGTERM', cleanExit );
+
+function getTask( name, extra )
+{
+	return require( './gulp-tasks/' + name )( Gulp, extra );
+}
+
+Gulp.task(
+	'build:less',
+	getTask( 'build-less' )
+);
+Gulp.task(
+	'watch:less',
+	getTask(
+		'watch-less',
+		[
+			'build:less',
+		]
+	)
+);
+
+Gulp.task(
+	'build:sass',
+	getTask( 'build-sass' )
+);
+Gulp.task(
+	'watch:sass',
+	getTask(
+		'watch-sass',
+		[
+			'build:sass',
+		]
+	)
+);
